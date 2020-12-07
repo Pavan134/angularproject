95cbd3c5cc844ba35eae15a396a75e3e6c93eced
pavan kumar <pavannerella28@gmail.com>
2020-12-07 18:02:00 +0530

    Third task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,14 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
